f3b18a02b02a1aed45e849e536ac4b1b14021707
unknown <al.korn95@gmail.com>
2018-03-31 20:45:23 +0300

    2

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.json())
   .put('/map', (req, res) => {
-    if( fs.existsSync(req.params.name + "")){
-      var text = fs.readFileSync(req.params.name + "", "utf8");
+    if( fs.existsSync(req.body.name + "")){
+      var text = fs.readFileSync(req.body.name + "", "utf8");
       var t = text.split("\n");
       fs.writeFileSync(req.body.name+"",req.body.h + "\n" + req.body.m + "\n" + req.body.s + "\n" + req.body.lat + "\n" + req.body.lon + "\n" + req.body.bat +"\n"+t[0]+"\n"+t[1]+"\n"+t[2]+"\n"+t[3]+"\n"+t[4]);
       }
-      else
+      else{
       fs.writeFileSync(req.body.name+"",req.body.h + "\n" + req.body.m + "\n" + req.body.s + "\n" + req.body.lat + "\n" + req.body.lon + "\n" + req.body.bat +"\n0\n0\n0\n0\n0\n0\n0");
     
+    }
     var s=req.body.name+"";
     res.json({msg:s});
 })
